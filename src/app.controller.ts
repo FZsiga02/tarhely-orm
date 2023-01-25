6e681f419ece2dda653e508f745f42349228da7a
FZsiga02 <fzsigmond02@gmail.com>
2023-01-25 13:25:01 +0100

    Entity

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
---
+++ src/tarhely.entity.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Tarhely {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nev: string;
+
+    @Column('int', { default: 500})
+    meret: number;
+
+    @Column('int', { default: 10000 })
+    ar: number;
+}
